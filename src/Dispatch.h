079ddad5d315f3685c89ce5f2b6a65ac0fb7254c
wicked-rainman <40294416+wicked-rainman@users.noreply.github.com>
2022-12-08 14:19:31 +0000

    Add files via upload

---
+++ src/Dispatch.h
@@ -0,0 +1,27 @@
+#ifndef Dispatch_h
+#define Dispatch_h
+
+#define KILLME (millis() >= _GlobalKillTime_)
+#include <Arduino.h>
+class Dispatch {
+public:
+  void run();
+  int add(void (*Fptr)(), unsigned long, unsigned long);
+  void remove(void (*Fptr)());
+  bool expired();
+  unsigned long runtime();
+  unsigned long delaytime();
+private:
+  struct RunTable {
+    unsigned long ScheduleInterval = 0;
+    unsigned long LastRunTime = 0;
+    unsigned long MaxRunTime = 0;
+    void (*Fptr)();
+    struct RunTable *next=NULL;
+  };
+  struct RunTable *_DispatchList_ = NULL;
+  unsigned long _GlobalKillTime_=0;
+  unsigned long _GlobalStartTime_=0;
+  unsigned long _GlobalOverrunTime_=0;
+};
+#endif
