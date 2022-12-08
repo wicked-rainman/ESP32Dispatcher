d58fae03dca4f28b1f7ef277bc8da5cc9089492e
wicked-rainman <40294416+wicked-rainman@users.noreply.github.com>
2022-12-08 14:24:42 +0000

    Update test.ino

--- examples/test/test.ino
+++ examples/test/test.ino
@@ -1,46 +1,69 @@
-//-----------------------------------------------------------
-// Simple Arduino sketch to show how the dispatcher library
-// can be used.
-// Author: WR
-//==========================================================
+//----------------------------------------------------------------------------------
+// Demo of Dispatch class
+//
+// Two functions (Function1 and Function2) are added to the Dispatch task queue.
+// Both are run every two seconds. Function1 expires after 10Ms, and Function2 
+// after 2000Ms (2 Seconds), 
+// When Function1 has run 4 times, it deletes itself rom the task queue and
+// adds Function3.
+// When Function3 has run 3 times, it deletes itself, which just leaves
+// Function2 being run by itself.
+//----------------------------------------------------------------------------------
 
+#include <M5Core2.h>
 #include <Dispatch.h>
 Dispatch myjobs;  //Create a dispatch instance
 
 void setup() {
   Serial.begin(115200);               //Set up the serial port Etc
-  myjobs.add(Function1, 2000, 5);   //Add Function1 to the list. Run every 2 seconds, expire after 5Ms
-  myjobs.add(Function2, 5000, 1000);  //Add Function2 to the list. Run every 5 seconds, expire after 1 second
-  myjobs.add(Function3, 120000, 0);   //Add Function3 to the list. Run after 2 minutes, no expiry time.
+  myjobs.add(Function1, 2000, 10);    //Add Function1 to the list. Run every 5 seconds, expire after 500 milliseconds
+  myjobs.add(Function2, 2000, 2000);  //Add Function2 to the list. Run every 5 seconds, expire after 500 milliseconds
+
+  delay(5000);                        //First execution of functions will be late
+  Serial.println("----Starting-----");
 }
 void loop() {
   myjobs.run();
 }
-void Function1() {
-  for (int k = 0; k < 5000000; k++) {
-    Serial.printf("%d ", k);
+void Function1() {  
+  static int x = 0;
+  while (1) {
+    delay(10);
     if (myjobs.expired()) {
-      Serial.printf("\nFunction1 expired\n");
-      return;
+      Serial.printf("Function 1 expired, Delayed by %luMs, ran for %luMs, x=%d\n", myjobs.delaytime(), myjobs.runtime(), x);
+      x++;
+      if (x == 4) {
+        Serial.print("Function 1 replaced by function3\n");
+        myjobs.remove(Function1);
+        myjobs.add(Function3, 2000, 10);
       }
+      return;
+    }
   }
 }
+
 void Function2() {
-  for (int k = 0; k < 5000; k++) {
-    if (myjobs.expired()) return;
-    int x = k;
+  while (1) {
+    delay(10);
+    if (myjobs.expired()) {
+      Serial.printf("Function 2 expired, Delayed by %luMs, ran for %luMs\n", myjobs.delaytime(), myjobs.runtime());
+      return;
+    }
   }
-  Serial.printf("In function 2. Ran after a %luMs delay. Took %luMs to complete\n",
-                myjobs.delaytime(), myjobs.runtime());
 }
 
 void Function3() {
-  int k;
-  for (k = 0; k < 5; k++) {
-    Serial.println("Hello");
-  }
-  if (k > 2) {
-    myjobs.remove(Function3);
-    Serial.println("Function3 had run and will never be seen again");
+  static int y=0;
+  while (1) {
+    delay(10);
+    if (myjobs.expired()) {
+      y++;
+      Serial.printf("Function 3 expired, Delayed by %luMs, ran for %luMs, x=%d\n", myjobs.delaytime(), myjobs.runtime(), y);
+      if(y==3) {
+        Serial.print("Function 3 removed\n");
+        myjobs.remove(Function3);
+      }
+      return;
+    }
   }
 }
